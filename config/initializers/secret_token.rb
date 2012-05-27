77a11c9ca89a75377f452fcb04b4e21cb979a7dd
Student <student@CA-iMac-05.local>
2012-05-26 23:27:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApprenticeJobs::Application.config.secret_token = 'd3ad8fedee18aef2f4a5d1bcb3e1c19a9c74d87b9784f1932ade5c9edb0ea26400751d6cf9ec0fb3645b5375782a00acf3f23edc5e6617ee99e07384ec165718'
